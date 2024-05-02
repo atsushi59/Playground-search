2acca2f81658257605291faea5103ff3f42c92d5
atsushi59 <atsushiyamada59@gmail.com>
2024-05-02 15:35:35 +0900

    GPT-4の検索結果取得と渡す形の変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,38 @@
 class SearchesController < ApplicationController
-  def index
+  def search
+    api_key = ENV['OPENAI_API_KEY']
+    chatgpt_service = ChatgptService.new(api_key)
+
+    selected_transport = params[:selected_transport]
+    selected_time = params[:selected_time]
+    selected_age = params[:selected_age]
+    selected_activity = params[:selected_activity]
+    address = params[:address]
+    
+    user_input = "#{address}から#{selected_transport}で#{selected_time}以内で#{selected_age}の子供が対象の#{selected_activity}できる場所を正式名称で2件提示してください"
+    
+    messages = [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: user_input }
+    ]
+
+    response = chatgpt_service.create_chat_completion(messages)
+
+    if response.success?
+      @result = response.parsed_response
+      message_content = @result['choices'].first['message']['content']
+
+      places = message_content.split("\n").select { |line| line.match(/^\d+\./) }
+      @answer = places.join("\n")
+      session[:query] = @answer
+      #redirect_to index_path
+      puts "Answer: #{answer}"
+    else
+      @error_message = "Error: #{response.parsed_response['error']['message']}"
+      puts error_message
+    end
   end
 
-  def search
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+
   post "search", to: "search#index"
-  
+
 end
