7b0bf0a24f59fffdef77ab52a8337ee158442f92
atsushi59 <atsushiyamada59@gmail.com>
2024-05-02 09:34:44 +0900

    Searches_controllerの作成

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,7 @@
+class SearchesController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root "static_pages#index"
   # Defines the root path route ("/")
   # root "posts#index"
+  root "static_pages#index"
+  resources :searches, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
+  
 end
