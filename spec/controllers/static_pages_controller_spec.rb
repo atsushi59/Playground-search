045d535fd83d6df9623640d52feae8f2e6380111
atsushi59 <atsushiyamada59@gmail.com>
2024-05-07 11:09:20 +0900

    google_places_service_Rspec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
-  describe "GET #view" do
-    it "returns http success" do
-      get :view
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
