d75d8060df1d0011e82b4561265985f51dedf52c
atsushi59 <atsushiyamada59@gmail.com>
2024-05-11 22:59:54 +0900

    エラーメッセージの変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -61,7 +61,6 @@ class SearchesController < ApplicationController
   end
 
   def check_search_limit
-    return unless Rails.env.production?
     ip_address = request.remote_ip
     today_search_count = SearchLog.where(ip_address: ip_address)
                                   .where("created_at >= ?", Time.zone.now.beginning_of_day)
