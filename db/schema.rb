056c703c55766fb359aedf2936764c3f18f99bfc
atsushi59 <atsushiyamada59@gmail.com>
2024-05-15 19:51:15 +0900

    Lineログイン実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_033952) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_090239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_033952) do
     t.index ["ip_address"], name: "index_search_logs_on_ip_address"
   end
 
+  create_table "sns_credentials", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,4 +73,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_033952) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "sns_credentials", "users"
 end
