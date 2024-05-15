12e8a65370aa5e4d70c5a358ac04851de8b0b2fb
atsushi59 <atsushiyamada59@gmail.com>
2024-05-16 01:11:04 +0900

    googleログイン実装

--- Gemfile
+++ Gemfile
@@ -60,7 +60,7 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mswin mswin64 mingw x64_mingw]
-  gem 'dotenv-rails'
+  gem 'dotenv-rails', groups: [:development, :test]
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'pry-rails'
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,25 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, only: [:google_oauth2, :line]
 
     def line
         @user = User.from_omniauth(request.env["omniauth.auth"])
-    
         if @user.persisted?
             sign_in_and_redirect @user, event: :authentication
             set_flash_message(:notice, :success, kind: "LINE") if is_navigational_format?
         else
             session["devise.line_data"] = request.env["omniauth.auth"].except("extra")
+            redirect_to new_user_registration_url
+        end
+    end
+    
+    def google_oauth2
+
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        if @user.persisted?
+            sign_in_and_redirect @user, event: :authentication
+            set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+        else
+            session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
             redirect_to new_user_registration_url
         end
     end
