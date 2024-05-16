6963dcff9496b591764855decdf4346574538c5c
atsushi59 <atsushiyamada59@gmail.com>
2024-05-16 22:28:47 +0900

    本番環境でのパスワード変更

--- Gemfile
+++ Gemfile
@@ -88,6 +88,7 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
   gem 'selenium-webdriver'
+  gem 'mail'
 end
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'dotenv/load' if Rails.env.development? || Rails.env.test? || Rails.env.production?
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
