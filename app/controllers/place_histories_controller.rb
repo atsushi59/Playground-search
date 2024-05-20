e547809e0e96adaff09878f9e14d6c70663a7c38
atsushi59 <atsushiyamada59@gmail.com>
2024-05-20 10:21:44 +0900

    検索ページ以外からもここに行くボタンで保存

--- app/controllers/place_histories_controller.rb
+++ app/controllers/place_histories_controller.rb
@@ -3,18 +3,25 @@ class PlaceHistoriesController < ApplicationController
     before_action :set_place, only: [:create]
 
     def index
+        @activity_type_options = Place.distinct.pluck(:activity_type).compact
         @place_histories = current_user.place_histories.includes(:place).order(created_at: :desc).page(params[:page]).per(10)
+    
+        if params[:address_cont].present? || params[:activity_type_eq].present? || params[:keyword].present?
+            @place_histories = @place_histories.joins(:place).where("places.address LIKE ?", "%#{params[:address_cont]}%") if params[:address_cont].present?
+            @place_histories = @place_histories.joins(:place).where("places.activity_type = ?", params[:activity_type_eq]) if params[:activity_type_eq].present?
+            if params[:keyword].present?
+                keyword = "%#{params[:keyword]}%"
+                @place_histories = @place_histories.joins(:place).where("places.name LIKE ? OR places.address LIKE ?", keyword, keyword)
+            end
+        end
     end
 
     def create
         @place_history = current_user.place_histories.build(place: @place)
         destination = params[:destination] || root_path
 
-        if @place_history.new_record?
-            @place_history.save
-        end
-        
-        redirect_to destination, allow_other_host: true
+        @place_history.save
+        render json: { redirect_to: destination }
     end
 
     private
