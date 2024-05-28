0a461dd20fe3901bb9ae5f4b2616174db1c2b08f
atsushi59 <atsushiyamada59@gmail.com>
2024-05-28 13:26:02 +0900

    place_history_rspec

--- spec/controllers/place_histories_controller_spec.rb
+++ spec/controllers/place_histories_controller_spec.rb
@@ -3,4 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe PlaceHistoriesController, type: :controller do
+    let(:user) { create(:user) }
+    let(:place) { create(:place, user: user) }
+    let!(:place_history) { create(:place_history, user: user, place: place) }
+
+    before do
+        sign_in user
+    end
+
+    describe 'GET #index' do
+        render_views
+
+        it 'リクエストが成功すること' do
+            get :index
+            expect(response).to be_successful
+        end
+
+        it '訪問履歴の場所が@placesに割り当てられること' do
+            get :index
+            expect(response.body).to include(place.name)
+            expect(response.body).to include(place.address)
+        end
+
+        it '@place_historiesが正しく割り当てられること' do
+            get :index
+            expect(response.body).to include(place.name)
+            expect(response.body).to include(place.address)
+        end
+    end
+
+    describe 'POST #create' do
+        it '訪問履歴が作成されること' do
+            expect do
+                post :create, params: { place_id: place.id, destination: '/some_destination' }, format: :json
+            end.to change { PlaceHistory.count }.by(0)
+        end
+
+        it '正しいJSONレスポンスが返されること' do
+            post :create, params: { place_id: place.id, destination: '/some_destination' }, format: :json
+            json = JSON.parse(response.body)
+            expect(json['redirect_to']).to eq('/some_destination')
+        end
+    end
 end
