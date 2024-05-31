56b6cfe6e396bdb5606cf8ce5bca3649993533c1
atsushi59 <atsushiyamada59@gmail.com>
2024-05-31 15:02:51 +0900

    ヘッダーの修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications_as_visited.order(created_at: :desc).page(params[:page]).per(10)
+    @notifications = current_user.notifications_as_visited.includes(:visitor, :visited, :review, :comment).order(created_at: :desc).page(params[:page]).per(10)
     @notifications.where(read: false).each do |notification|
       notification.update(read: true)
     end
