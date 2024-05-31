87e2386f65c082904ced9681c6428297051e7702
atsushi59 <atsushiyamada59@gmail.com>
2024-05-31 17:52:02 +0900

    JSコードのエラー修正

--- app/javascript/views/form_search.js
+++ app/javascript/views/form_search.js
@@ -2,7 +2,11 @@ document.addEventListener('turbo:load', function() {
     var form = document.getElementById('location_form');
     var modal = document.getElementById('my_modal_2');
 
-    form.addEventListener('submit', function(event) {
-        modal.showModal();
-    });
+    if (form && modal) {
+        form.addEventListener('submit', function(event) {
+            modal.showModal();
+        });
+    } else {
+        console.error('Form or modal not found');
+    }
 });
