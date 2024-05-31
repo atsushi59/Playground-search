f1b50805aefdc366f2c40beb2c8662cf1449b14f
atsushi59 <atsushiyamada59@gmail.com>
2024-05-31 12:32:04 +0900

    行った履歴ページのページネーション

--- app/models/place_history.rb
+++ app/models/place_history.rb
@@ -5,8 +5,4 @@ class PlaceHistory < ApplicationRecord
   belongs_to :place
 
   validates :user_id, uniqueness: { scope: :place_id }
-
-  scope :by_place_ids, lambda { |place_ids|
-    where(place_id: place_ids).includes(:place).order(created_at: :desc)
-  }
 end
