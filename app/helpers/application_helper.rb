ae403a586d6addc05e7326a0710a809f16d7b73f
atsushi59 <atsushiyamada59@gmail.com>
2024-06-07 15:45:04 +0900

    Open_Graphの編集

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,13 +41,13 @@ module ApplicationHelper
         description: 'あそびばさがそを使えば、子供の遊べる場所を簡単に検索し、ルート案内します',
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.png'),
+        image: image_url('ogp.png') + "?#{Time.now.to_i}",
         locale: 'ja_JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@',
-        image: image_url('ogp1.png'),
+        image: image_url('ogp.png') + "?#{Time.now.to_i}",
       }
     }
   end
